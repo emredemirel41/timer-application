ac35d767ffd8ed6d0aa22940d6b9d7121ce1c444
emre demirel <emredemire4196@gmail.com>
2023-03-20 02:00:45 +0300

    fix vuetify

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
 
 
 export default defineConfig({
     plugins: [
         vue(),
+        vuetify({ autoImport: true }), // Enabled by default
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
