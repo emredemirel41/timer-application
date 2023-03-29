2763fe5e44e14fc49e116f0efb6b5ff238aa0303
emre demirel <emredemire4196@gmail.com>
2023-03-29 22:54:48 +0300

    add a cors policy

--- config/cors.php
+++ config/cors.php
@@ -16,19 +16,12 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_origins' => ['http://127.0.0.1:8000','http://localhost:8080', 'https://remoly.io'],
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['Content-Type', 'X-Requested-With'],
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 
 ];
